f9fc52ca506ec58b5d3964dc9b03a5ba06170c31
Dappdevv <dappdevv@gmail.com>
2025-02-06 15:02:29 +0300

    up

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -4,6 +4,22 @@ import { SyncDatabase } from '@pilcrowjs/db-query';
 import type { SyncAdapter } from '@pilcrowjs/db-query';
 
 const sqlite = sqlite3('sqlite.db');
+sqlite.exec(`
+	CREATE TABLE user (
+    id INTEGER NOT NULL PRIMARY KEY,
+    github_id INTEGER NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    username TEXT NOT NULL
+);
+
+CREATE INDEX github_id_index ON user(github_id);
+
+CREATE TABLE session (
+    id TEXT NOT NULL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES user(id),
+    expires_at INTEGER NOT NULL
+);
+  `);
 
 const adapter: SyncAdapter<sqlite3.RunResult> = {
 	query: (statement: string, params: unknown[]): unknown[][] => {
